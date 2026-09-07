fbe5c8bc355580cf5518099121c9ca63bcecb63b
aryanargamingboy-ctrl <aryanargamingboy@gmail.com>
2026-09-07 09:07:43 +0530

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# API & Bot Details
+API_ID = 36935840
+API_HASH = "bee7b7cdf449914c0c3e14176795951"
+BOT_TOKEN = "8976408844:AAGlmvfx26Oy1qVF_12hB7n0ZCWvcTBF6NM"
+
+# Database & String Session
+MONGO_DB_URI = "mongodb+srv://rajrubi494_db_user:pass854108@mybotmusic.aicn2f6.mongodb.net/?appName=Mybotmusic"
+STRING_SESSION = "BQCZzqEAHhy5ByexhXd_KmSOfSa_AoaKMNPPnB7v-T7X6VvqIEInWhxRM9fobzxg3ROPCCNwULGlYjJZfzNX4sr4jdJfWeJ-pSNP5sPI8yz5L2MTeOoVtY7hMCwy5RILtd2DEStpz9T5sltTFkF-ofZiV-A1morrVCREt6cc4UR1zhwcIH8bXcKGzjdeioo9VVwP3sjlhDJ_FmQVGKXcnZ0FMseC_CHG1hBFRXoRzjxPuoYP3dgHE7qu_fgLSKaYK0qRkNQeF_C0n7f-40pBFgvzfW4m0us1jf_YY-nhGkVG-n8mdVL_MBVWpi9CiQZuWPyzCp6COFY2yuQVBRxKuZjOMgo7EQAAAAIUX8ZNAA"
+
+# Branding Settings
+MUSIC_BOT_NAME = "TGxMUSIC"
+OWNER_ID = 8369944379
+SUPPORT_GROUP = "https://t.me/+W3dCcCPDJesxNjU1"
+START_IMG_URL = "https://telegra.ph/file/af7061d47f9f257d09a5b.jpg" # Sample music image
+
+# Default Variables
+DURATION_LIMIT_MIN = 180
+PLAYLIST_FETCH_LIMIT = 25
+CLEANMODE_DELETE_MINS = 5
